01066b776f89a1f19c0241752187efa0afd0ffcd
trustmycode <vlad.verkhovtsev@gmail.com>
2023-09-04 09:43:59 +0300

    solved task 'Только для богачей'

--- src/main/java/com/javarush/task/task08/task0818/Solution.java
+++ src/main/java/com/javarush/task/task08/task0818/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task08.task0818;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /*
@@ -9,12 +10,30 @@ import java.util.Map;
 public class Solution {
 
   public static Map<String, Integer> createMap() {
-    //напишите тут ваш код
-    return null;
+    Map<String, Integer> map = new HashMap<>();
+    map.put("Asdf", 500);
+    map.put("Qwer", 450);
+    map.put("Zxcv", 1500);
+    map.put("Qazw", 600);
+    map.put("Wsxe", 700);
+    map.put("Edcr", 300);
+    map.put("Zaqw", 340);
+    map.put("Xswe", 1000);
+    map.put("Rfds", 900);
+    map.put("Vcxz", 700);
+    return map;
   }
 
   public static void removeItemFromMap(Map<String, Integer> map) {
-    //напишите тут ваш код
+    Map<String, Integer> copyMap = new HashMap<>();
+    for (Map.Entry<String, Integer> entry : map.entrySet()) {
+      if (entry.getValue() < 500) {
+        copyMap.put(entry.getKey(), entry.getValue());
+      }
+    }
+    for (Map.Entry<String, Integer> entry : copyMap.entrySet()) {
+      map.remove(entry.getKey());
+    }
   }
 
   public static void main(String[] args) {
