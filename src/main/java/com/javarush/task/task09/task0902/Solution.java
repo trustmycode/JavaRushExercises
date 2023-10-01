100895ca32e020adf49975238ec53fac7dec7a2e
trustmycode <vlad.verkhovtsev@gmail.com>
2023-10-01 18:07:29 +0300

    9 уровень, лекция 2, 9.2

--- src/main/java/com/javarush/task/task09/task0902/Solution.java
+++ src/main/java/com/javarush/task/task09/task0902/Solution.java
@@ -12,38 +12,33 @@ public class Solution {
   public static String method1() {
     method2();
     StackTraceElement[] elements = Thread.currentThread().getStackTrace();
-    System.out.println(elements[elements.length - 1].getMethodName());
-    return elements[0].getMethodName();
+    return elements[2].getMethodName();
   }
 
   public static String method2() {
     method3();
     StackTraceElement[] elements = Thread.currentThread().getStackTrace();
-    System.out.println(elements[elements.length - 1].getMethodName());
-    return Thread.currentThread().getName();
+    return elements[2].getMethodName();
     //напишите тут ваш код
   }
 
   public static String method3() {
     method4();
     StackTraceElement[] elements = Thread.currentThread().getStackTrace();
-    System.out.println(elements[elements.length - 1].getMethodName());
-    return Thread.currentThread().getName();
+    return elements[2].getMethodName();
     //напишите тут ваш код
   }
 
   public static String method4() {
     method5();
     StackTraceElement[] elements = Thread.currentThread().getStackTrace();
-    System.out.println(elements[elements.length - 1].getMethodName());
-    return Thread.currentThread().getName();
+    return elements[2].getMethodName();
     //напишите тут ваш код
   }
 
   public static String method5() {
     StackTraceElement[] elements = Thread.currentThread().getStackTrace();
-    System.out.println(elements[elements.length - 1].getMethodName());
-    return Thread.currentThread().getName();
+    return elements[2].getMethodName();
     //напишите тут ваш код
   }
 }
