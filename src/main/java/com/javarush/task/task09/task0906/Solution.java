0b71bd94eb84c632a8f519b7f4c44e25d6dbdbb0
trustmycode <vlad.verkhovtsev@gmail.com>
2023-10-01 18:40:20 +0300

    9 уровень, лекция 2, 9.3 - 9.6

---
+++ src/main/java/com/javarush/task/task09/task0906/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task09.task0906;
+
+/*
+Логирование стек-трейса
+*/
+
+public class Solution {
+  public static void main(String[] args) {
+    log("In main method");
+  }
+
+  public static void log(String text) {
+    StackTraceElement[] elements = Thread.currentThread().getStackTrace();
+    System.out.println(
+        elements[2].getClassName()
+            + ": "
+            + elements[2].getMethodName()
+            + ": "
+            + text); // напишите тут ваш код
+  }
+}
